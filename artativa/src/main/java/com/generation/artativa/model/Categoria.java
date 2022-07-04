d863e442fd584309f3afa9bd78203ac741d1cae5
HericlisVO <hericlisventura.o@gmail.com>
2022-07-04 09:32:21 -0300

    Implementando as Classes Categoria, Categoria Repository e CategoriaController.

--- artativa/src/main/java/com/generation/artativa/model/Categoria.java
+++ artativa/src/main/java/com/generation/artativa/model/Categoria.java
@@ -1,5 +1,73 @@
 package com.generation.artativa.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name ="tb_categoria")
 public class Categoria {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 10, max = 255)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 10, max = 255)
+	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private Produto produto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	
+	
+	
+	
 
 }
