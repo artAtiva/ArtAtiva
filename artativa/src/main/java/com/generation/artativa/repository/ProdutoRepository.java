cd6baac0f6f4f0a2e60714482c9cfb1f8b9bb477
Diego Candido da Silva <ogeid.csilva@gmail.com>
2022-07-13 11:57:18 -0300

    Adicionando a última camada de UsuarioController

--- artativa/src/main/java/com/generation/artativa/repository/ProdutoRepository.java
+++ artativa/src/main/java/com/generation/artativa/repository/ProdutoRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.artativa.model.Produto;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+public interface ProdutoRepository extends JpaRepository
+<Produto, Long> {
 	
-	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+	public List<Produto> findAllByNomeContainingIgnoreCase
+	(String nome);
 }
