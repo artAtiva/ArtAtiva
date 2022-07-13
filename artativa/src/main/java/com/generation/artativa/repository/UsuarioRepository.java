8526fe0a409b7b9db27d7f0d939b3f52a827867b
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-13 10:35:05 -0300

    classes usuario, pacote seguranca, classe basicSec e UserDetaImp

--- artativa/src/main/java/com/generation/artativa/repository/UsuarioRepository.java
+++ artativa/src/main/java/com/generation/artativa/repository/UsuarioRepository.java
@@ -1,6 +1,6 @@
 package com.generation.artativa.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +10,6 @@ import com.generation.artativa.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
-	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	public Optional<Usuario> findByUsuario(String usuario);
 	
 }
