342bc67fb5c6108ada282282877baa39bc151869
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-07-18 09:24:05 -0300

    adicionando @schema

--- artativa/src/main/java/com/generation/artativa/model/Usuario.java
+++ artativa/src/main/java/com/generation/artativa/model/Usuario.java
@@ -19,6 +19,8 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table (name = "tb_usuario")
 public class Usuario {
@@ -31,6 +33,7 @@ public class Usuario {
 	@Size(min = 3, max = 255)
 	private String nome;
 	
+	@Schema(example = "email@email.com.br")
 	@NotBlank(message = "O atributo Usuário é Obrigatório!")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
